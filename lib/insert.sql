9361aa9bc9b8c7171ca444036f238f08b0eeec60
Nathaniel-Kerr <NathanielKerr10@gmail.com>
2020-12-02 22:54:24 -0800

    commit tree e7492d3e32aee82eed600ba7437ee6013d347662

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 20, "M", "mocah", "agressive", true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 10, "M", "black", "rowdy", true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 6, "F", "red/brown", "unstable", true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 19, "M", "NULL", "PTSD", true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 13, "F", "white", "even", true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 2, "M", "green", "goofy", false);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 6, "F", "red", "full of life", true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 20, "M", "black", "NULL", true);
